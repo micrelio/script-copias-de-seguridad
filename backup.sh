8efe2e2c811bf0e2d2202300e3733539ed0fa22a
unknown <microa4@gmail.com>
2020-08-18 17:58:04 +0200

    no va

--- backup.sh
+++ backup.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-tar cvfz /home/juegos.tgz /home/tcagame/
-tar cvfz /home/webs.tgz /var/www/
+DATE=$(date +%Y-%m-%d-%H%M%S)
+BACKUP_DIR="home/backup/"
+SOURCE="/home/tcagame/"
+SOURCE2="/var/www/"
+tar -cvzpf $BACKUP_DIR/juegos-$DATE.tgz $SOURCE
+tar -cvzpf $BACKUP_DIR/webs-$DATE.tgz $SOURCE2
+
+
+
+
