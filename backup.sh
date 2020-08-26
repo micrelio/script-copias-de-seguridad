b011b04da1962b4893df53a8f26012c1190286f0
unknown <microa4@gmail.com>
2020-08-26 16:58:15 +0200

    reparaciones

--- backup.sh
+++ backup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d-%H%M%S)
-BACKUP_DIR="home/backup/"
-SOURCE="/home/tcagame/"
-SOURCE2="/var/www/"
-tar -cvzpf $BACKUP_DIR/juegos-$DATE.tgz $SOURCE
-tar -cvzpf $BACKUP_DIR/webs-$DATE.tgz $SOURCE2
+BACKUP_DIR="home/backups"
+SOURCE="home/tcagame/"
+SOURCE2="var/www/"
+tar -cvzpf $BACKUP_DIR/juegos/juegos-$DATE.tgz $SOURCE
+tar -cvzpf $BACKUP_DIR/webs/webs-$DATE.tgz $SOURCE2
 
 
 
