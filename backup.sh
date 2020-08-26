7bb67d3079edd270b80be06dd27c3a717119a042
unknown <microa4@gmail.com>
2020-08-26 17:05:31 +0200

    añadidas copias de mysql

--- backup.sh
+++ backup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 DATE=$(date +%Y-%m-%d-%H%M%S)
-BACKUP_DIR="home/backups"
-SOURCE="home/tcagame/"
-SOURCE2="var/www/"
+BACKUP_DIR="/home/backups"
+SOURCE="/home/tcagame"
+SOURCE2="/var/www"
 tar -cvzpf $BACKUP_DIR/juegos/juegos-$DATE.tgz $SOURCE
 tar -cvzpf $BACKUP_DIR/webs/webs-$DATE.tgz $SOURCE2
-
-
-
-
+mysqldump -v --opt --events --routines --triggers --default-character-set=utf8 -u root --password=TxcjAoGQCXz9 titaniumgamers > $BACKUP_DIR/sql/titaniumgamers_-$DATE.sql
+mysqldump -v --opt --events --routines --triggers --default-character-set=utf8 -u root --password=TxcjAoGQCXz9 foro > $BACKUP_DIR/sql/foro_-$DATE.sql
